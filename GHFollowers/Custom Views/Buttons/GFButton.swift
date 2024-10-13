f67713f50b7d935392a0f85f1bc7a58e206a01a1
mshayan-experlabs <138456600+mshayan-experlabs@users.noreply.github.com>
2024-10-13 11:57:08 +0500

    update project iOS target version to 15, add button configurations to GFButton, provided by iOS

--- GHFollowers/Custom Views/Buttons/GFButton.swift
+++ GHFollowers/Custom Views/Buttons/GFButton.swift
@@ -23,26 +23,27 @@ class GFButton: UIButton {
     }
     
     // Custom init
-    convenience init(backgroudColor: UIColor, title: String) {
+    convenience init(color: UIColor, title: String, systemImageName: String) {
         self.init(frame: .zero)
         
-        self.backgroundColor = backgroudColor
-        self.setTitle(title, for: .normal)
+        set(color: color, title: title, systemImageName: systemImageName)
     }
     
     private func configure() {
-        // Layer is responsible for drawing the view content.
-        layer.cornerRadius = 10
-        setTitleColor(.white, for: .normal)
-        // Configurable font size to be dynamic
-        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        configuration = .tinted()
+        configuration?.cornerStyle = .medium
         // To use autolayout.
         translatesAutoresizingMaskIntoConstraints = false
     }
     
-    func set(backgroundColor: UIColor, title: String) {
-        self.backgroundColor = backgroundColor
-        setTitle(title, for: .normal)
+    func set(color: UIColor, title: String, systemImageName: String) {
+        configuration?.baseBackgroundColor = color
+        configuration?.baseForegroundColor = color
+        configuration?.title = title
+        
+        configuration?.image = UIImage(systemName: systemImageName)
+        configuration?.imagePadding = 6
+        configuration?.imagePlacement = .leading
     }
     
 }
