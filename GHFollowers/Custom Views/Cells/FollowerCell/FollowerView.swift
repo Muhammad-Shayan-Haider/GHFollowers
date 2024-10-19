bde130e92fc394bdef9b06bb1d7c33e93480f3ab
mshayan-experlabs <138456600+mshayan-experlabs@users.noreply.github.com>
2024-10-19 10:55:46 +0500

    implement SwiftUI view for follower cell for iOS 16 only

---
+++ GHFollowers/Custom Views/Cells/FollowerCell/FollowerView.swift
@@ -0,0 +1,33 @@
+//
+//  FollowerView.swift
+//  GHFollowers
+//
+//  Created by Muhammad Shayan on 19/10/2024.
+//
+
+import SwiftUI
+
+struct FollowerView: View {
+    var follower: Follower
+    
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: follower.avatarUrl)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } placeholder: {
+                Image("avatar-placeholder")
+            }
+            .clipShape(.circle)
+        }
+        Text(follower.login)
+            .bold()
+            .lineLimit(1)
+            .minimumScaleFactor(0.6)
+    }
+}
+
+#Preview {
+    FollowerView(follower: Follower(login: "SeanAllen", avatarUrl:  ""))
+}
